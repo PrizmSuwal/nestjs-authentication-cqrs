2bf2d7eb9056dc6106beb925be65147442e74982
Prism Suwal <prismsuwal@pop-os.localdomain>
2021-11-25 19:06:12 +0545

    UserController-IndependentOf-UserServices

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,18 +1,29 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UsersService } from '../services/users.service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { usersDTO } from '../users.dto';
+import { AddUserCommand } from '../commands/impl/add-user.command';
+import { GetAllUsers } from '../queries/get-user';
+import { usersMock } from '../users.mock';
 
 @Controller('users')
 export class UsersController {
-    constructor (private usersService: UsersService){}
+    constructor (
+        private commandBus: CommandBus,
+        private queryBus: QueryBus
+    ){}
+    public users = usersMock;
 
     @Post('add')
     async addUser(@Body() users: usersDTO) {
-        return this.usersService.addUser(users);
+        return await this.commandBus.execute(
+            new AddUserCommand(users),
+        );
     }
 
     @Get('list')
     async listUser(){
-        return this.usersService.listUser();
+        return await this.queryBus.execute(
+            new GetAllUsers(this.users),
+        )
     }
 }
