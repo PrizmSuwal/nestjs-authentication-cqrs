472f1d94221bd8c16f9294cf59809045552c5c70
Prism Suwal <prismsuwal@pop-os.localdomain>
2021-11-29 10:34:13 +0545

    Services_Removed

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -4,24 +4,26 @@ import { usersDTO } from '../users.dto';
 import { AddUserCommand } from '../commands/impl/add-user.command';
 import { GetAllUsers } from '../queries/get-user';
 import { usersMock } from '../users.mock';
-import { UsersService } from '../users.service';
 
 @Controller('users')
 export class UsersController {
     constructor (
         private commandBus: CommandBus,
         private queryBus: QueryBus,
-        private userservice: UsersService
     ){}
     public users = usersMock;
 
     @Post('add')
     async addUser(@Body() users: usersDTO) {
-        return await this.userservice.addUser(users);
+        return await this.commandBus.execute(
+            new AddUserCommand(users),
+        );
     }
 
     @Get('list')
     async listUser(){
-        return await this.userservice.listUser();
+        return await this.queryBus.execute(
+            new GetAllUsers(this.users),
+        );
     }
 }
